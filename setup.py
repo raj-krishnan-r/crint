2a1314e2522ef4c0d88ec25a9c66575d25293955
Raj Krishnan R <rkrishnan1993@live.com>
2024-05-30 22:43:30 +0400

    version updated

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='crint',
-    version='0.1.0',
+    version='0.1.1',
     description='A simple module for colored terminal output',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
